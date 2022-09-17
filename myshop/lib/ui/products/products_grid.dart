439f48509d5219471f96e69d2e5ce33aa973331e
John <johndoe@example.com>
2022-09-17 23:23:00 +0700

    xay dung trang cac san pham nguoi dung

--- myshop/lib/ui/products/products_grid.dart
+++ myshop/lib/ui/products/products_grid.dart
@@ -9,19 +9,20 @@ class ProductsGrid extends StatelessWidget {
   const ProductsGrid(this.showFavorites, {super.key});
 
   @override
-    Widget build(BuildContext context) {
-      final productsManager = ProductsManager();
-      final products =
-        showFavorites ? productsManager.favoriteItems: productsManager.items; 
-        return GridView.builder(
-          padding: const EdgeInsets.all(10.0), 
-          itemCount: products.length,
-          itemBuilder: (ctx, i) => ProductGridTile (products[i]), gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            childAspectRatio: 3/2,
-            crossAxisSpacing: 10, 
-            mainAxisSpacing: 10,
-          ),
-      );
+  Widget build(BuildContext context) {
+    final productsManager = ProductsManager();
+    final products =
+        showFavorites ? productsManager.favoriteItems : productsManager.items;
+    return GridView.builder(
+      padding: const EdgeInsets.all(10.0),
+      itemCount: products.length,
+      itemBuilder: (ctx, i) => ProductGridTile(products[i]),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+      ),
+    );
   }
 }
